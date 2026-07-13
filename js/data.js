19054b2ee42da9ec09af49880c5288222ebdc25e
Divyanshu-crv <crvidyapeeth@gmail.com>
2026-07-14 02:00:17 +0530

    few sec add

--- js/data.js
+++ js/data.js
@@ -6,8 +6,23 @@ const quickAccess = [
     },
 
     {
+        name: "Notion",
+        url: "https://www.notion.com/"
+    },
+
+    {
         name: "CodePen",
         url: "https://codepen.io/pen"
+    },
+
+    {
+        name: "Excalidraw",
+        url: "https://excalidraw.com/"
+    },
+
+    {
+        name: "Boilerplates",
+        url: "https://divyanshu-crv.github.io/snippet/"
     }
 
 ];
@@ -15,7 +30,48 @@ const quickAccess = [
 const sections = [
 
     {
-        title: "Documentation",
+        title: "Important",
+        icon: "🔒",
+
+        categories: [
+
+            {
+                title: "Some Important",
+
+                subcategories: [
+
+                    {
+                        title: "Updation",
+
+                        items: [
+
+                            {
+                                name: "Website Update",
+                                url: "./assets/Collection_03update.pdf",
+                                type: "link"
+                            },
+
+                            {
+                                name: "Random(Perplexity)",
+                                url: "https://www.perplexity.ai/",
+                                type: "link"
+                            }
+
+                        ]
+
+                    }
+
+                ]
+
+            }
+
+        ]
+    },
+
+    // =======================================
+
+    {
+        title: " EX 3 layers",
         icon: "📚",
 
         categories: [
@@ -87,7 +143,7 @@ const sections = [
     },
 
     {
-        title: "Bookmarks",
+        title: " EX 2 layers",
         icon: "🔖",
 
         categories: [
@@ -124,32 +180,259 @@ const sections = [
 
         ]
     },
-// ---------------------------------------------------------------
+    // ---------------------------------------------------------------
     {
-        title: "Important",
-        icon: "🔒",
+        title: "  GitHub",
+        icon: "🚀",
 
         categories: [
 
             {
-                title: "Some Important",
+                title: "MY GitHub Things",
 
                 subcategories: [
 
                     {
-                        title: "Updation",
+                        title: "📂 Projects",
 
                         items: [
 
                             {
-                                name: "Website Update",
-                                url: "./assets/Collection_03update.pdf",
+                                name: "Career Command Center",
+                                url: "https://divyanshu-crv.github.io/First-Exam-Durign-MCA/",
                                 type: "link"
                             },
 
                             {
-                                name: "Random(Perplexity)",
+                                name: "To-Do List",
+                                url: "https://divyanshu-crv.github.io/to-do/",
+                                type: "link"
+                            },
+
+                            {
+                                name: "MediaHub",
+                                url: "https://divyanshu-crv.github.io/MediaHub/",
+                                type: "link"
+                            },
+
+                            {
+                                name: "Math",
+                                url: "https://divyanshu-crv.github.io/math/",
+                                type: "link"
+                            }
+
+                        ]
+
+                    },
+
+                    {
+                        title: "YT Links",
+
+                        items: [
+
+                            {
+                                name: "About GitHub",
+                                url: "https://youtu.be/yBebbcNBhRc?si=O51NloG4I_J88uS5",
+                                type: "link"
+                            }
+
+                        ]
+
+                    },
+
+                    {
+                        title: "Update CMD",
+
+                        items: [
+
+                            {
+                                name: "Update Commands",
+                                url: "./assets/gitupdate.txt",
+                                type: "file"
+                            }
+
+                        ]
+
+                    },
+
+                    {
+                        title: "🧩 Code Sharing",
+
+                        items: [
+
+                            {
+                                name: "CodePen",
+                                url: "https://codepen.io/pen",
+                                type: "link"
+                            },
+
+                            {
+                                name: "PasteCode",
+                                url: "https://pastecode.io/",
+                                type: "link"
+                            },
+
+                            {
+                                name: "CodePen",
+                                url: "https://codepen.io/pen",
+                                type: "link"
+                            }
+
+                        ]
+
+                    },
+
+                    {
+                        title: "Draw",
+
+                        items: [
+
+                            {
+                                name: "Excalidraw",
+                                url: "https://excalidraw.com/",
+                                type: "link"
+                            }
+
+                        ]
+
+                    },
+
+                    {
+                        title: "Helpful Repositories",
+
+                        items: [
+
+                            {
+                                name: "#",
+                                url: "#",
+                                type: "link"
+                            }
+
+                        ]
+
+                    },
+
+                ]
+
+            }
+
+
+
+        ]
+    },
+
+
+    {
+        title: "Resources",
+        icon: "🛍️",
+
+        categories: [
+
+            {
+                title: "Collections",
+
+                subcategories: [
+
+                    {
+                        title: "Mix Chrome Bookmarks",
+
+                        items: [
+
+                            {
+                                name: "Unsplash",
+                                url: "https://unsplash.com/",
+                                type: "link"
+                            },
+
+                            {
+                                name: "Image Downloader",
+                                url: "https://www.toolsbug.com/images-downloader-online.php",
+                                type: "link"
+                            },
+
+                            {
+                                name: "4K Wallpaper Collection",
+                                url: "https://www.reddit.com/r/wallpapers/comments/1csgawf/ultra_high_quality_4k_elitist_wallpapers/",
+                                type: "link"
+                            },
+
+                            {
+                                name: "Perplexity",
                                 url: "https://www.perplexity.ai/",
+                                type: "link"
+                            },
+
+                            {
+                                name: "Fathers Link",
+                                url: "https://divyanshu-t.github.io/father-link/",
+                                type: "link"
+                            },
+
+                            {
+                                name: "Quantam Coding",
+                                url: "https://github.com/QuantumCoding123",
+                                type: "link"
+                            },
+
+                            {
+                                name: "File CR",
+                                url: "https://filecr.com/home/",
+                                type: "link"
+                            },
+
+                            {
+                                name: "I Love Pdf",
+                                url: "https://www.ilovepdf.com/",
+                                type: "link"
+                            },
+
+                            {
+                                name: "Discord",
+                                url: "https://discord.com/channels/@me",
+                                type: "link"
+                            },
+
+                            {
+                                name: "Notbooklm",
+                                url: "https://notebooklm.google/",
+                                type: "link"
+                            },
+
+                            {
+                                name: "Arena",
+                                url: "https://arena.ai/",
+                                type: "link"
+                            },
+
+                            {
+                                name: "Remixicon",
+                                url: "https://remixicon.com/",
+                                type: "link"
+                            },
+
+                            {
+                                name: "Rajbasha",
+                                url: "http://rajbhasha.net/download2/index.php/GoogleInputToolsHindi.exe",
+                                type: "link"
+                            },
+
+                            {
+                                name: "Uk Indeed",
+                                url: "https://uk.indeed.com/",
+                                type: "link"
+                            },
+                        ]
+
+                    },
+
+                    {
+                        title: "VS Code",
+
+                        items: [
+
+                            {
+                                name: "Emmet Cheat Sheet",
+                                url: "https://docs.emmet.io/cheat-sheet/",
                                 type: "link"
                             }
 
@@ -161,7 +444,187 @@ const sections = [
 
             }
 
+
+
         ]
-    }
+    },
+
+    // -------------------------------------------------------------
+    {
+        title: " AI Related",
+        icon: "🤖",
+
+        categories: [
+
+            {
+                title: "🌐",
+
+                subcategories: [
+
+                    {
+                        title: "Prompts",
+
+                        items: [
+
+                            {
+                                name: "meigen.ai (img prompt)",
+                                url: "https://www.meigen.ai/",
+                                type: "link"
+                            },
+
+                            {
+                                name: "Prompt",
+                                url: "https://www.oyenino.com/?utm_source=ig&utm_medium=social&utm_content=link_in_bio&fbclid=PAZXh0bgNhZW0CMTEAc3J0YwZhcHBfaWQPOTM2NjE5NzQzMzkyNDU5AAGnPnXHH8IauMSqprZa_SS7fK2Po0naHqgQmrb9mqC-MXVyVBKIfItDLalbs80_aem_7yHb-YkxeRkxsH4QzFRk_Q",
+                                type: "link"
+                            }
+
+                        ]
+
+                    }
+
+
+                ]
+
+            }
+
+        ]
+    },
+
+    {
+        title: " Insta Related(update karna hai) ",
+        icon: "📸",
+
+        categories: [
+
+            {
+                title: "🌐",
+
+                subcategories: [
+
+                    {
+                        title: "Prompts",
+
+                        items: [
+
+                            {
+                                name: "meigen.ai (img prompt)",
+                                url: "https://www.meigen.ai/",
+                                type: "link"
+                            },
+
+                            {
+                                name: "Prompt",
+                                url: "https://www.oyenino.com/?utm_source=ig&utm_medium=social&utm_content=link_in_bio&fbclid=PAZXh0bgNhZW0CMTEAc3J0YwZhcHBfaWQPOTM2NjE5NzQzMzkyNDU5AAGnPnXHH8IauMSqprZa_SS7fK2Po0naHqgQmrb9mqC-MXVyVBKIfItDLalbs80_aem_7yHb-YkxeRkxsH4QzFRk_Q",
+                                type: "link"
+                            }
+
+                        ]
+
+                    }
+
+
+                ]
+
+            }
+
+        ]
+    },
+    {
+        title: "Masti ",
+        icon: "🕺",
+
+        categories: [
+
+            {
+                title: "🌐 Websites",
+
+                subcategories: [
+
+                    {
+                        title: "Movie",
+
+                        items: [
+
+                            {
+                                name: "Filmyfly",
+                                url: "https://www.google.com/search?q=filmyfly&oq=filmyfly&gs_lcrp=EgZjaHJvbWUqBwgAEAAYjwIyBwgAEAAYjwIyCggBEAAYsQMYgAQyBggCEEUYQTIGCAMQRRg8MgYIBBBFGDwyBggFEEUYPDIGCAYQRRg8MgYIBxAFGCzSAQk3OTE0ajBqMTWoAgywAgHxBTzuz9_D_oMo&sourceid=chrome&source=chrome.rb&ie=UTF-8",
+                                type: "link"
+                            },
+
+                            {
+                                name: "BollyFix",
+                                url: "https://www.google.com/search?q=bollyflix&oq=boll&gs_lcrp=EgZjaHJvbWUqBggAECMYJzIGCAAQIxgnMgYIARBFGDkyBggCECMYJzIKCAMQABixAxiABDIKCAQQABixAxiABDIHCAUQABiABDINCAYQABiDARixAxiABDINCAcQABiDARixAxiABDIHCAgQABiABDIHCAkQABiPAtIBCTI1NTdqMGoxNagCCbACAfEF7mX-ZI6aSpjxBe5l_mSOmkqY&sourceid=chrome&source=chrome.rb&ie=UTF-8",
+                                type: "link"
+                            },
+
+                            {
+                                name: "cinemalux",
+                                url:  "https://www.google.com/search?q=cinemalux&oq=&gs_lcrp=EgZjaHJvbWUqCQgCECMYJxjqAjIMCAAQIxgnGOoCGK0GMgkIARAjGCcY6gIyCQgCECMYJxjqAjIJCAMQIxgnGOoCMgkIBBAjGCcY6gIyDAgFECMYJxjqAhitBjIJCAYQIxgnGOoCMgwIBxAjGCcY6gIYrQYyGAgIEAAYQhiRAhi0AhjqAhiABBiKBRjnBjIYCAkQABhCGJECGLQCGOoCGIAEGIoFGOcGMhgIChAAGEIYkQIYtAIY6gIYgAQYigUY5wbSAQkxNDExajBqMTWoAguwAgHxBVe8KWtkRiL78QVXvClrZEYi-w&sourceid=chrome&source=chrome.rb&ie=UTF-8",
+                                type: "link"
+                            },
+
+                            {
+                                name: "moviesmod",
+                                url:  "https://www.google.com/search?q=moviesmod&oq=&gs_lcrp=EgZjaHJvbWUqDAgFECMYJxjqAhitBjIMCAAQIxgnGOoCGK0GMgkIARAjGCcY6gIyCQgCECMYJxjqAjIJCAMQIxgnGOoCMgkIBBAjGCcY6gIyDAgFECMYJxjqAhitBjIJCAYQIxgnGOoCMgwIBxAjGCcY6gIYrQYyGAgIEAAYQhiRAhi0AhjqAhiABBiKBRjnBjIYCAkQABhCGJECGLQCGOoCGIAEGIoFGOcGMhgIChAAGEIYkQIYtAIY6gIYgAQYigUY5wbSAQkyNTc1ajBqMTWoAguwAgHxBQj_9dN7F3Nv&sourceid=chrome&source=chrome.rb&ie=UTF-8",
+                                type: "link"
+                            },
+
+                            {
+                                name: "Downloadhub",
+                                url:  "https://www.downloadhub.press/",
+                                type: "link"
+                            },
+
+                            {
+                                name: "vegamovies",
+                                url:  "https://www.google.com/search?q=vegamovies&oq=vegamovies&gs_lcrp=EgZjaHJvbWUyBggAEEUYOTIGCAEQRRhBMgYIAhBFGDzSAQkxNTI1ajBqMTWoAgiwAgHxBULK3OsKfZoe8QVCytzrCn2aHg&sourceid=chrome&source=chrome.rb&ie=UTF-8",
+                                type: "link"
+                            }
+
+
+                        ]
+
+                    }
+
+
+                ]
+
+            },
+
+            {
+                title: " 🎶 youtube",
+
+                subcategories: [
+
+                    {
+                        title: " HINDI Songs",
+
+                        items: [
+
+
+                            {
+                                name: "Chaap Tilak Sab Chini Re",
+                                url:  "https://youtu.be/CaKXsXDLHhY?si=t1czrBg--DOVlI-T",
+                                type: "link"
+                            },
+
+                            {
+                                name: "Odhani Odh Ke Nachu ",
+                                url:  "https://youtu.be/RX3Xenjc-GE?si=uZ7DKTF7BgJeq-9p",
+                                type: "link"
+                            },
+
+                        ]
+
+                    }
+
+
+                ]
+
+            }
+
+        ]
+    },
+
 
 ];
